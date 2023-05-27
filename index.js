8cbfa866b6a90208d1b54e0f8170cb864ac1a2b4
spereira15 <105558705+spereira15@users.noreply.github.com>
2023-05-27 18:37:47 -0500

    created function to initialize

--- index.js
+++ index.js
@@ -51,7 +51,6 @@ const questions = [
     },
   ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
       if (err) {
@@ -63,7 +62,17 @@ function writeToFile(fileName, data) {
   }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+      .prompt(questions)
+      .then((answers) => {
+        const markdown = generateMarkdown.generateMarkdown(answers);
+        writeToFile('README.md', markdown);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
 // Function call to initialize app
 init();
